250f8f233ad4be97861f3932b5f477e28aa1933a
ohkhaila <khaila.jones@lessannoyingcrm.com>
2018-05-23 10:47:51 -0500

    Morning
    php practice

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+  <head>
+    <title>My Website</title>
+    <link rel= 'stylesheet' href='style.css'/>
+  </head>
+  <body>
+    <h1>Welcome to my website!</h1>
+      <p class = 'quote'>Thanks for visiting my site!</p>
+    <h2>About me</h2>
+      <p id = 'firstParagraph'>I'm a student in STL</p>
+    <h2>My Blog</h2>
+      <p>Here are some blog posts :)</p>
+      <ul>
+        <li><a href="fellowship/about.html">About Me</a></li>
+        <li><a href="fellowship/weather.html">What I Think About The Weather</a></li>
+      </ul>
+  </body>
+</html>
---
+++ php_practice/variable.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>PHP Variables Practice</title>
+    <!--maybe add a stylesheet? -->
+  </head>
+  <body>
+</html>
+<!--switch to PHP for dynamic features -->
+<?php
+//define variables
+  $hello = "Hello world!";
+  $name = "Khaila";
+  $age = 21;
+//display variables value
+echo $hello "My name is $name and I am $age years old."
+ ?>
