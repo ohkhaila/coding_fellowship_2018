e803c322e6546e355f4470fa1b180d1d427c3026
ohkhaila <khaila.jones@lessannoyingcrm.com>
2018-05-28 13:44:57 -0500

    gone

--- php_practice/variable.php
+++ php_practice/variable.php
@@ -1,6 +1,7 @@
 <html>
   <head>
     <title>PHP Variables Practice</title>
+<<<<<<< HEAD
     <link rel='stylesheet' href='style.css'/>
     
     <!--maybe add a stylesheet? -->
@@ -22,3 +23,18 @@
 
 
  </html>
+=======
+    <!--maybe add a stylesheet? -->
+  </head>
+  <body>
+</html>
+<!--switch to PHP for dynamic features -->
+<?php
+//define variables
+  $hello = "Hello world!";
+  $name = "Khaila";
+  $age = 21;
+//display variables value
+echo $hello "My name is $name and I am $age years old."
+ ?>
+>>>>>>> 250f8f233ad4be97861f3932b5f477e28aa1933a
