51600812f1a77870a2714f8c6bf4d474400549e9
ohkhaila <khaila.jones@lessannoyingcrm.com>
2018-06-19 11:46:05 -0500

    check
    checking if github will let me commit anything

---
+++ view/loginPage.php
@@ -0,0 +1,26 @@
+<?php
+include('init.php');
+
+if(isset($_REQUEST['submitLogin'])){
+	$errors = array();
+	if(!$_REQUEST['email']){
+		$errors['email'] = "Email is required";
+	}
+	if(!$_REQUEST['password']){
+		$errors['password'] = "Password is required";
+	}
+	if(sizeof($errors) == 0){
+		echo verifyUser($_REQUEST['email'], $_REQUEST['password']);
+		exit;
+	}
+}
+var_dump($_REQUEST);
+
+ ?>
+
+<form action = '' method = 'post' name = 'login'>
+	Email: 		<input type = 'text' name = 'email'/><br/>
+	Password: 	<input type = 'text' name = 'password'/><br/>
+				<input type = 'submit' name = 'submitLogin' value = 'login'/>
+</form>
+<a href = '/view/signUp.php'>Sign Up Here!</a>
